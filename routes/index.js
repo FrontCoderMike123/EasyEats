db6d83d19bf676f9d077271d33a5e723d046f5b5
FrontCoderMike123 <michelBeaubien@hotmail.com>
2016-02-16 01:16:02 -0800

    Getting this App Going!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('pages/login', {
-    title: 'Please Login'
+    title: 'Please Login',
+    formTitle: 'Welcome Back',
+    username: 'Username',
+    password: 'Password',
+    craving: 'Hungry for Something Else?',
+    Login: 'Login'
   });
 });
 
@@ -11,6 +16,12 @@ router.get('/signUp', function(req, res) {
   res.render('pages/signUp', {
     title: 'Register Today',
     comment: 'Start Eating Now!'
+  });
+});
+
+router.get('/budget', function(req, res) {
+  res.render('pages/budget', {
+    title: 'Welcome Back!'
   });
 });
 
