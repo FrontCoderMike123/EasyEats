3a11a2daa5b81050d0c71319628427e236a08fb7
FrontCoderMike123 <michelBeaubien@hotmail.com>
2016-02-16 23:22:14 -0800

    Registration form

---
+++ models/account.js
@@ -0,0 +1,18 @@
+(function() {
+
+	var mongoose = require('mongoose');
+	var Schema = mongoose.Schema;
+	var passportLocalMongoose = require('passport-local-mongoose');
+
+	var Account = new Schema({
+		username: String,
+		password: String,
+		firstname: String,
+		lastname: String
+	});
+
+	Account.plugin(passportLocalMongoose);
+
+	module.exports = mongoose.model('Account', Account);
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account.js');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
+    res.render('pages/index', {
+      user : req.user,
+      title: 'New or Not?'
+    });
+});
+
+router.get('/login', function(req, res) {
   res.render('pages/login', {
+    user: req.user,
     title: 'Please Login',
     formTitle: 'Welcome Back',
     username: 'Username',
     password: 'Password',
     Login: 'Login'
   });
+});
+
+router.post('/login', passport.authenticate('local'), function(req,res) {
+  res.redirect('/budget');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 router.get('/signUp', function(req, res) {
@@ -19,8 +38,34 @@ router.get('/signUp', function(req, res) {
     password: 'Password',
     fname: 'First Name',
     lname: 'Last Name',
-    signUp: 'Sign Up'
+    signUp: 'Sign Up',
+    info: ''
   });
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+router.post('/signUp', function(req, res) {
+    Account.register(new Account({ username : req.body.username, firstname: req.body.firstname, lastname: req.body.lastname }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render("pages/signUp", {
+              info: "Sorry. That username already exists. Try again.",
+              title: 'Register Today',
+              signUpTitle: 'Sign Up',
+              username: 'Username',
+              password: 'Password',
+              fname: 'First Name',
+              lname: 'Last Name',
+              signUp: 'Sign Up'
+            })
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
 });
 
 router.get('/budget', function(req, res) {
@@ -30,7 +75,8 @@ router.get('/budget', function(req, res) {
     craving: 'Hungry for Something Else?',
     budget: 'Enter Budget',
     placeholder: '($)',
-    find: 'Find Food'
+    find: 'Find Food',
+    info: 'Hello '+req.user.username+', feeling hungry?'
   });
 });
 
